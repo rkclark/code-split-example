b55fc5e87ca490189877820cf623bc83fd734d39
rkclark <rkingsleyclark@gmail.com>
2020-12-18 10:36:10 +0000

    Make pages

---
+++ src/pages/journey/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./index.css";
+
+export default function Journey() {
+    return (
+        <div className="journey">
+            <h1>Journey</h1>
+        </div>
+    );
+}
---
+++ src/pages/renewals/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./index.css";
+
+export default function Renewals() {
+    return (
+        <div className="renewals">
+            <h1>Renewals</h1>
+        </div>
+    );
+}
